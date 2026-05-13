c3561baafd4e49c2332e4d54361d9e5d9d09145d
Starfironal <ronalstarfi@gmail.com>
2026-05-13 12:06:11 -0400

    chore: remove temporary database query scratchpad file

--- scratch_query.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=sistema_fondas', 'root', 'PARALELEPIPEDO3312'); 
-$triggers = $db->query("SHOW TRIGGERS LIKE 'solicitud'")->fetchAll(PDO::FETCH_ASSOC);
-print_r($triggers);
-?>
