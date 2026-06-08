80f8e70a062765c85ae023d27114bdbf52f7fcd7
Daycoro <daicyechezuria1@gmail.com>
2026-06-08 14:21:46 -0400

    Cambio en el sistema

--- config/database.php
+++ config/database.php
@@ -10,6 +10,11 @@ class Database
     /// $password = "PARALELEPIPEDO3312";
     public $conn;
 
+    public function __construct()
+    {
+        date_default_timezone_set('America/Caracas');
+    }
+
     public function getConnection()
     {
         $this->conn = null;
@@ -21,6 +26,10 @@ class Database
             );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("SET NAMES utf8mb4");
+
+            // Asegurar que MySQL use la misma zona horaria que el servidor PHP.
+            $timezoneOffset = date('P');
+            $this->conn->exec("SET time_zone = '{$timezoneOffset}'");
         } catch (PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
             $this->conn = null;
